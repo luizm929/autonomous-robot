a95ef7953c6f1b559578073c4d3d079eb764a7fe
Luis Martinez <luizmartines@gmail.com>
2013-08-16 21:31:52 -0700

    Update neopix.ino

--- robotProjectV6_2_1/neopix.ino
+++ robotProjectV6_2_1/neopix.ino
@@ -1,7 +1,7 @@
 
 //Distance LED uses strip2 
 void DistanceColorSwitch(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<40; i++) {
+  for(uint16_t i=0; i<30; i++) {
       strip2.setPixelColor(i, c);
       strip2.show();
       delay(wait);
@@ -10,7 +10,7 @@ void DistanceColorSwitch(uint32_t c, uint8_t wait) {
 
 //Temp LED uses strip 
 void TempColorSwitch(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<40; i++) {
+  for(uint16_t i=0; i<30; i++) {
       strip.setPixelColor(i, c);
       strip.show();
       delay(wait);
@@ -18,4 +18,4 @@ void TempColorSwitch(uint32_t c, uint8_t wait) {
 }
 
 //Notice we reduced the for loop so that it takes less
-//time to pulse and therefore better for use with ultrasonic sensro.
+//time to pulse and therefore better for use with ultrasonic sensor.
